c7c1dccde186c15a7ba4d891de32234af5fabf58
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-08-27 17:17:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+    
+}
+ 
+
+function setup(){
+canvas=createCanvas(300,300)
+canvas.center();
+video=createCapture(VIDEO);
+video.hide;
+}
+
+
+function draw(){
+image(video)
+
+}
